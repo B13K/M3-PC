d2d1e520fa0da6c19afecea42452b3d2b3f743f6
Jonathan <jonathan_3_7@hotmail.com>
2023-01-18 05:34:34 -0500

    HW Express

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,6 +4,77 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+function $Promise(executor){
+    if(typeof executor !== "function"){
+        throw TypeError ("executor must be a function")
+    }
+
+    this._state = "pending"
+    this._value = undefined
+    this._handlerGroups = []
+
+    
+    const callHandlers = (value) => {
+        // this._handlerGroups.forEach(current => {
+        //     this._state === 'fulfilled' && current.successCb && current.successCb()
+        //     this._state === 'rejected' && current.errorCb && current.errorCb()
+        // })
+
+        while(this._handlerGroups.length > 0){
+            const currentHandler = this._handlerGroups.shift();
+            this._state === 'fulfilled' &&
+                            currentHandler.successCb &&
+                            currentHandler.successCb(value);
+            this._state === 'rejected' &&
+                            currentHandler.errorCb &&
+                            currentHandler.errorCb(value);
+        }
+     }
+
+    this._internalResolve = (someData) => {
+        
+        if(this._state !== 'pending') return;
+        this._state = 'fulfilled'
+        this._value = someData
+        callHandlers(someData)
+        
+    }
+    this._internalReject = (myReason) => {
+        if(this._state !== 'pending') return;
+        this._state = "rejected"
+        this._value = myReason;
+        callHandlers(myReason)
+    }
+
+    let resolve = (resolve) => {this._internalResolve(resolve)}
+    let reject = (reject) => {this._internalReject(reject)}
+
+    executor(resolve,reject)
+
+
+     this.then = (success, error) => {
+        let successCb = typeof success === 'function' 
+                        ? success
+                        : false
+        let errorCb = typeof error === 'function'
+                        ? error
+                        : false
+        let downstreamPromise = new $Promise(()=>{})
+        this._handlerGroups.push({successCb: successCb,
+                                    errorCb:errorCb,
+                                    downstreamPromise})
+
+        this._state !== 'pending' && callHandlers(this._value)
+
+        return downstreamPromise;
+     }
+
+     this.catch = (myFunc) => {
+        return this.then(null, myFunc)
+     }
+
+}
+
 
 
 module.exports = $Promise;
--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -5,13 +5,154 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+let posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
-// server.use(express.json());
+server.use(express.json());
 
 // TODO: your code to handle requests
 
+let id = 1
 
+server.post("/posts", function(req, res) {
+    let { author, title, contents } = req.body;
+    if(!author || !title || !contents){
+        res.status(STATUS_USER_ERROR).json( {error: "No se recibieron los parámetros necesarios para crear el Post"})
+    }
+    else{
+        const post = {id:id++, author, title, contents};
+        posts.push(post);
+        res
+            .status(200)
+            .json(post)
+
+    }
+})
+
+server.post("/posts/author/:author", (req, res) => {
+    const { title, contents} = req.body;
+    const {author} = req.params;
+
+    if(!title || !contents) {
+        res
+            .status(STATUS_USER_ERROR)
+            .json({error: "No se recibieron los parámetros necesarios para crear el Post"})
+    }
+    else {
+        
+        const post = {id:id++, author, title, contents};
+        posts.push(post);
+        res
+            .status(200)
+            .json(post)
+    }
+})
+
+server.get("/posts", (req, res) => {
+    const {term} = req.query;
+    if(term){
+        res
+            .status(200)
+            .json(posts.filter(p => p.title.includes(term) === true || p.contents.includes(term) === true))
+    }
+    else{
+        res
+            .status(200)
+            .json(posts)
+    }
+    
+})
+
+server.get("/posts/:author", (req, res) => {
+    const { author } = req.params;
+
+    let authorPosts = posts.filter(p => p.author === author)
+
+    if(authorPosts.length){
+        res
+            .status(200)
+            .json(authorPosts)
+    }
+    else{
+        res
+            .status(STATUS_USER_ERROR)
+            .json({error: "No existe ningun post del autor indicado"})
+    }
+})
+
+server.get("/posts/:author/:title", (req, res) => {
+    const { author, title } = req.params;
+
+    let authorTitle = posts.filter(p => p.author===author && p.title=== title)
+
+    if(authorTitle.length){
+        res
+            .status(200)
+            .json(authorTitle)
+    } else {
+        res
+            .status(STATUS_USER_ERROR)
+            .json({error: "No existe ningun post con dicho titulo y autor indicado"})
+    }
+
+})
+
+server.put("/posts", (req, res) => {
+    const {id, title, contents} = req.body;
+
+    if(!id || !title || !contents){
+        res
+            .status(STATUS_USER_ERROR)
+            .json({error: "No se recibieron los parámetros necesarios para modificar el Post"})
+    }
+
+    let postId = posts.filter(p => p.id === id)
+
+    if(postId.length){
+        postId[0].title = title;
+        postId[0].contents = contents;
+
+        res
+            .status(200)
+            .json(postId[0])
+    } else {
+        res
+        .status(STATUS_USER_ERROR)
+        .json({error: "No se encunetra los parámetros necesarios para modificar el Post"})
+    }
+})
+
+server.delete("/posts", (req, res) => {
+    const {id} = req.body;
+
+    const post = posts.find(p => p.id === parseInt(id))
+
+    if(!id || !post){
+        res.status(STATUS_USER_ERROR).json({error: "Mensaje de error"})
+    }
+    else{
+        
+        posts = posts.filter(p => p.id !== id);
+
+        res.status(200).json({ success: true })
+    }
+
+})
+
+
+server.delete("/author", (req, res) => {
+    const { author } = req.body
+
+    const post = posts.find(p => p.author===author)
+
+    if(!author || !post){
+        res.status(STATUS_USER_ERROR).json({"error": "No existe el autor indicado"})
+    }
+    else{
+        let deleteAuthor = posts.filter(p => p.author===author)
+        posts = posts.filter(p => p.author!==author)
+        res.status(200).json(deleteAuthor)
+    }
+})
 module.exports = { posts, server };
